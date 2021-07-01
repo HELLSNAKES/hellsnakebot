4c46ca7427a7bc8bd3c5aff5ba10bebee394f926
HELLSNAKE <63496374+HELLSNAKES@users.noreply.github.com>
2021-07-01 19:39:32 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+const { Client, Collection } = require("discord.js");
+const config = require("./config.json");
+const fs = require("fs");
+
+const client = new Client({
+disableEveryone: true
+});
+client.commands = new Collection();
+client.aliases = new Collection();
+client.categories = fs.readdirSync("./commands/");
+["command"].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
+});
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity(config.activity); 
+});
+client.on("message", async message => {
+    console.log(`[${new Date(Date.now())}] ${message.author.username} (${message.author.id}) issued command in ${message.channel.id}: ${message.content}`)
+    const prefix = (config.prefix);
+    if (message.author.bot) return;
+    if (!message.guild) return;
+    if (!message.content.startsWith(prefix)) return;
+    if (!message.member) message.member = await message.guild.fetchMember(message);
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    if (cmd.length === 0) return;
+    let command = client.commands.get(cmd);
+    if (!command) command = client.commands.get(client.aliases.get(cmd));
+    if (command) 
+        command.run(client, message, args);
+});
+
+client.login(config.token);
