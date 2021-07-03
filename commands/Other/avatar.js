7805cd5660083717c6b7b4eb10d3d96739841b29
HELLSNAKE <63496374+HELLSNAKES@users.noreply.github.com>
2021-07-04 06:56:27 +0700

    create avatar.js.

---
+++ commands/Other/avatar.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "avatar",
+    category: "Other",
+    description: "Get user avatar",
+    usage: "[command | user] or [command]",
+    run: async(client, message, args) => {
+
+        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.slice(0).join(' ') || x.user.username === args[0]) || message.member;
+
+        if (!member.user.avatarURL) return message.channel.send(`That user does not have an avatar!!!`);
+
+        const avatar = new Discord.MessageEmbed()
+            .setTitle(`${member.user.username}'s Avatar`)
+            .setColor("RANDOM")
+            .setImage(member.user.avatarURL())
+            .setColor(member.displayHexColor)
+            .setImage(member.user.displayAvatarURL({ dynamic: true, size: 2048 }))
+            .setURL(member.user.avatarURL())
+        message.channel.send(avatar)
+    }
+};
