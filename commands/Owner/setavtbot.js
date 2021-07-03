56721da7fea341a07a1d7c501decc0887c765660
HELLSNAKE <63496374+HELLSNAKES@users.noreply.github.com>
2021-07-03 10:57:51 +0700

    Create setavtbot.js

---
+++ commands/Owner/setavtbot.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const { Admin } = require('../../config.json');
+const client = new Discord.Client()
+
+module.exports = {
+    name: "setavatartbot",
+    category: "Owner",
+    description: "Set avatar bot",
+    usage: "[command +url]",
+    run: async (client, message, args) => {
+   let avatarurl = args.join(" ");
+   if(message.author.id !== Admin) return message.channel.send('Insufficient permission!!')
+   client.user.setAvatar(`${avatarurl}`)
+   if (!avatarurl) return message.channel.send(`Usage: setavtbot <url>`)
+   let embed = new Discord.MessageEmbed()
+       .setTitle('New Avatar Set')
+       .setImage(`${avatarurl}`)
+       .setTimestamp()
+    message.channel.send(embed)
+    .catch(e => {
+        console.log(e)
+        return message.channel.send("Something Went Wrong!")
+    })
+  }
+}
