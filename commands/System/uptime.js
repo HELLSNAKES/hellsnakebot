39f2fad9c5ee84d3e58bdbc7ad197aa0b295290f
HELLSNAKE <63496374+HELLSNAKES@users.noreply.github.com>
2021-07-03 21:20:36 +0700

    Create uptime.js

---
+++ commands/System/uptime.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+
+module.exports ={
+    name: 'uptime',
+    description: "Uptime the bot",
+    category: "System",
+    usage: "[command]",
+     run: async (client, message, args) => {     
+     let days = Math.floor(client.uptime / 86400000);
+     let hours = Math.floor(client.uptime / 3600000) % 24;
+     let minutes = Math.floor(client.uptime / 60000) % 60;
+     let seconds = Math.floor(client.uptime / 1000) % 60;
+     const uptimeembed = new Discord.MessageEmbed()
+     .setTitle('HELLSNAKE-BOT')
+     .setColor('RED')
+     .addField(':computer: UPTIME', ` ${days}days ${hours}hrs ${minutes}min ${seconds}sec`, true)	    
+     .setTimestamp(Date())
+     message.channel.send(uptimeembed);
+    }
+}
