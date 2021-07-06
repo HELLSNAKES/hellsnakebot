bbc06661052da61fce0dac3c9cc8efa7eceacfba
HELLSNAKE <63496374+HELLSNAKES@users.noreply.github.com>
2021-07-06 10:49:20 +0700

    Create clear.js

---
+++ commands/Moderation/clear.js
@@ -0,0 +1,28 @@
+const { MessageEmbed } = require('discord.js')
+module.exports = {
+    name: "clear",
+    category: "Moderation",
+    description: "delete message",
+    usage: "[COMMAND] + [amount]",
+    run: async (client, message, args) => {
+        if (!message.member.permissions.has("MANAGE_MESSAGES"))
+            return message.channel.send(`Insufficient permission!!`);
+        if (!args[0]) {
+            return message.channel.send(`Please enter a amount 1 to 100`)
+        }
+        let deleteAmount;
+        if (parseInt(args[0]) > 100 ) {
+            deleteAmount = 100;
+        } else {
+            deleteAmount = parseInt(args[0]);
+        }
+        await message.channel.bulkDelete(deleteAmount, true);
+        const embed = new MessageEmbed()
+            .setTitle(`${message.author.username}`)
+            .setThumbnail(message.author.displayAvatarURL())
+            .setDescription(`successfully deleted ${deleteAmount}`)
+            .setFooter(message.author.username, message.author.displayAvatarURL())
+            .setColor('RANDOM')
+        await message.channel.send(embed)
+    }
+}
