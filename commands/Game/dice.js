b57bb1dcea611e950e91f409fe4a8adf82a4036b
HELLSNAKE <63496374+HELLSNAKES@users.noreply.github.com>
2021-07-08 17:22:02 +0700

    🎲

---
+++ commands/Game/dice.js
@@ -0,0 +1,19 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "dice",
+    category: "Game",
+    description: 'Dice roll command!',
+    usage: "[command]",
+    run: async (client, message, args) => {
+        const rolls = ["1", "2", "3", "4", "5", "6"];
+        const roll = rolls[Math.floor(Math.random() * rolls.length)];
+        let newEmbed = new MessageEmbed()
+        .setTitle("🎲 Dice Roll!")
+        .setThumbnail(message.author.displayAvatarURL())
+        .setColor("RANDOM")
+        .setDescription(`You rolled a **${roll}**!`)
+        .setTimestamp()
+        message.channel.send(newEmbed)
+    }   
+}
