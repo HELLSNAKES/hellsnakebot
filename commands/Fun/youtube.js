cece5944d906244dad38b258f00ac39bd3887e24
HELLSNAKE <63496374+HELLSNAKES@users.noreply.github.com>
2021-07-22 08:08:19 +0700

    yt together

---
+++ commands/Fun/youtube.js
@@ -0,0 +1,23 @@
+const discord = require('discord.js')
+const { DiscordTogether } = require("discord-together");
+
+module.exports = {
+    name: "youtube",
+    aliases: ["yt"],
+    category: "Fun",
+    description: "Watch YouTube with your friends, on discord!",
+    usage: "[command]",
+    run: async(client, message, args) => {
+        client.discordTogether = new DiscordTogether(client);
+        if (message.member.voice.channel) {
+            client.discordTogether
+                .createTogetherCode(message.member.voice.channelID, "youtube")
+                .then(async (invite) => {
+                    return message.channel.send(`${invite.code}`);
+                })
+        } else {
+            message.channel.send('You need to be in a voice channel');
+        }
+    }
+}
+       
