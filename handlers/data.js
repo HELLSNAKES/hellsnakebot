7fae493849e3df49d21695f6cbcbc48543fd71bc
NaOH-dev <77104443+CuSO4-c3c@users.noreply.github.com>
2021-07-22 13:48:48 +0700

    data file

--- handlers/data.js
+++ handlers/data.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 var data = JSON.parse(fs.readFileSync('./database/data.json').toString())
 var datadir = __dirname + '/../database/data.json'
 module.exports = async (client) => {
-    console.log('Pushing data into client...')
+    console.log('[Data Handlers]', 'Pushing data into client...')
     client.data = data
     client.updateData = async () => {
         fs.writeFileSync(datadir, JSON.stringify(client.data, null, 4))
     }
-    console.log('Push data complete.')
+    console.log('[Data Handlers]', 'Push data complete.')
 }
