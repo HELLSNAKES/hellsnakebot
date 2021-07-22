58d83322bd47170d2df4f3aa33a1c82888536439
NaOH-dev <77104443+CuSO4-c3c@users.noreply.github.com>
2021-07-22 14:46:32 +0700

    ensure that the bot run after update, new way to add missing value into config.json

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const { Client, Collection, MessageEmbed } = require("discord.js");
 const fs = require("fs");
-
+const childProcess = require('child_process')
 if (!fs.existsSync('./config.json')) {
   console.log("config.json doesn't exists. Attemping to create a new one...")
   fs.writeFileSync('./config.json', `{
@@ -25,174 +25,172 @@ if (!fs.existsSync('./database/data.json')) {
   console.log('owo, data file not found.')
   fs.writeFileSync('./database/data.json', '{}')
 }
-
 var config = JSON.parse(fs.readFileSync('./config.json').toString());
-
-const configvalue = ['autoUpdate', 'token', 'prefix', 'Admin', 'osuAPI', 'youtubeAPI']
-configvalue.forEach(async (a) => {
-  if (config[a] == undefined) {
-    console.log(a, 'was not found in config.json. Adding with default value...')
-    if (a == 'autoUpdate') {
-      config.autoUpdate = true
-    }
-    if (a == 'token') {
-      config.token = ''
-    }
-    if (a == 'youtubeAPI') {
-      config.youtubeAPI = ''
-    }
-    if (a == 'Admin') {
-      config.Admin = ''
-    }
-    if (a == 'prefix') {
-      config.prefix = '!'
-    }
-    if (a == 'osuAPI') {
-      config.osuAPI = {
-        "client_id": "",
-        "client_secret": "",
-        "typeof client_id": "number",
-        "typeof client_secret": "string"
-      }
-    }
-    await fs.writeFileSync('./config.json', JSON.stringify(config, null, 4), {
-      mode: 0o666,
-      recursive: true
-    })
-  }
-})
-
-const moment = require('moment');
-const ms = require('ms');
-const Timeout = new Collection();
-let skip = false
-const childProcess = require('child_process')
-async function update() {
-  if (config.autoUpdate == true) {
-    console.log('[Git]', 'Updating...')
-    childProcess.spawn('git', ['pull'], {
-      stdio: 'inherit'
-    })
-  }
+const defaultconfig = {
+  "autoUpdate": true,
+  "token": "",
+  "prefix": "!",
+  "Admin": "",
+  "osuAPI": {
+    "client_id": "",
+    "client_secret": "",
+    "typeof client_id": "number",
+    "typeof client_secret": "string"
+  },
+  "youtubeAPI": ""
 }
-update()
-const client = new Client({
-  disableEveryone: true
-});
-require('discord-buttons')(client);
-client.commands = new Collection();
-client.aliases = new Collection();
-client.categories = fs.readdirSync("./commands/");
-["command", 'data'].forEach(handler => {
-  require(`./handlers/${handler}`)(client);
-});
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  setInterval(() => {
-    const statuses = [
+for (let a in defaultconfig) {
+  if (config[a] == undefined) {
+    console.log(a, 'was not found in config.json. Adding with defautl value' + defaultconfig[a] + '...')
+    config[a] = defaultconfig[a]
+  }
+  fs.writeFileSync('./config.json', JSON.stringify(config, null, 4))
+}
+var config = JSON.parse(fs.readFileSync('./config.json').toString());
+const main = async () => {
+  if (config.token != '') {
+  const moment = require('moment');
+  const ms = require('ms');
+  const Timeout = new Collection();
+
+  const client = new Client({
+    disableEveryone: true
+  });
+  require('discord-buttons')(client);
+  client.commands = new Collection();
+  client.aliases = new Collection();
+  client.categories = fs.readdirSync("./commands/");
+  ['data', "command"].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
+  });
+  client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    setInterval(() => {
+      const statuses = [
         `github.com/hellsnakes/hellsnakebot`,
         `HELLSNAKEBOT| ${config.prefix}help`,
         `osu!`,
-    ]
+      ]
 
-    const status = statuses[Math.floor(Math.random() * statuses.length)]
-    client.user.setActivity(status, { type: "PLAYING"})}, 5000) 
-});
-client.on("message", async message => {
-  const prefix = (config.prefix);
-  if (message.content.startsWith(prefix)) {
-    console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message.author.username} (${message.author.id}) issued command in ${message.channel.id}: ${message.content}`);
-  } else {
-    if (message.attachments.first() != undefined && message.content != '') {
-      console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message.author.username} (${message.author.id}) messaged in ${message.channel.id}: ${message.content}`);
-      console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message.author.username} (${message.author.id}) sent an attachment in ${message.channel.id}: ${message.attachments.first().url}`)
-    } else if(message.attachments.first() != undefined && message.content == '') {
-      console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message.author.username} (${message.author.id}) sent an attachment in ${message.channel.id}: ${message.attachments.first().url}`)
-    } else if(message.attachments.first() == undefined && message.content != '') {
-      console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message.author.username} (${message.author.id}) messaged in ${message.channel.id}: ${message.content}`);
-    } else ;
+      const status = statuses[Math.floor(Math.random() * statuses.length)]
+      client.user.setActivity(status, { type: "PLAYING" })
+    }, 5000)
+  });
+  client.on("message", async message => {
+    const prefix = (config.prefix);
+    if (message.content.startsWith(prefix)) {
+      console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message.author.username} (${message.author.id}) issued command in ${message.channel.id}: ${message.content}`);
+    } else {
+      if (message.attachments.first() != undefined && message.content != '') {
+        console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message.author.username} (${message.author.id}) messaged in ${message.channel.id}: ${message.content}`);
+        console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message.author.username} (${message.author.id}) sent an attachment in ${message.channel.id}: ${message.attachments.first().url}`)
+      } else if (message.attachments.first() != undefined && message.content == '') {
+        console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message.author.username} (${message.author.id}) sent an attachment in ${message.channel.id}: ${message.attachments.first().url}`)
+      } else if (message.attachments.first() == undefined && message.content != '') {
+        console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message.author.username} (${message.author.id}) messaged in ${message.channel.id}: ${message.content}`);
+      } else;
+    }
+    if (message.author.bot) return;
+    if (!message.guild) return;
+    if (!message.content.startsWith(prefix)) return;
+    if (!message.member) message.member = await message.guild.fetchMember(message);
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    if (cmd.length === 0) return;
+    let command = client.commands.get(cmd);
+    if (!command) command = client.commands.get(client.aliases.get(cmd));
+    if (command)
+      if (Timeout.has(`${command.name}${message.author.id}`)) return message.channel.send(`You are on a \`${ms(Timeout.get(`${command.name}${message.author.id}`) - Date.now(), { long: true })}\` cooldown.`)
+    command.run(client, message, args);
+    Timeout.set(`${command.name}${message.author.id}`, Date.now() + command.timeout)
+    setTimeout(() => {
+      Timeout.delete(`${command.name}${message.author.id}`)
+    }, command.timeout)
+  });
+  if (!fs.existsSync('./database/xp.json')) {
+    fs.mkdirSync('./database', {
+      mode: 0o777,
+      recursive: true
+    })
+    fs.appendFileSync('./database/xp.json', '{}')
   }
-  if (message.author.bot) return;
-  if (!message.guild) return;
-  if (!message.content.startsWith(prefix)) return;
-  if (!message.member) message.member = await message.guild.fetchMember(message);
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  const cmd = args.shift().toLowerCase();
-  if (cmd.length === 0) return;
-  let command = client.commands.get(cmd);
-  if (!command) command = client.commands.get(client.aliases.get(cmd));
-  if (command)
-    if (Timeout.has(`${command.name}${message.author.id}`)) return message.channel.send(`You are on a \`${ms(Timeout.get(`${command.name}${message.author.id}`) - Date.now(), { long: true })}\` cooldown.`)
-  command.run(client, message, args);
-  Timeout.set(`${command.name}${message.author.id}`, Date.now() + command.timeout)
-  setTimeout(() => {
-    Timeout.delete(`${command.name}${message.author.id}`)
-  }, command.timeout)
-});
-if (!fs.existsSync('./database/xp.json')) {
-  fs.mkdirSync('./database', {
-    mode: 0o777,
-    recursive: true
-  })
-  fs.appendFileSync('./database/xp.json', '{}')
-}
-const xpfile = require('./database/xp.json');
-client.on("message", function (message) {
-  if (message.author.bot) return;
-  var addXP = Math.floor(Math.random() * 8) + 3
+  const xpfile = require('./database/xp.json');
+  client.on("message", function (message) {
+    if (message.author.bot) return;
+    var addXP = Math.floor(Math.random() * 8) + 3
 
-  if (!xpfile[message.author.id]) {
-    xpfile[message.author.id] = {
-      xp: 0,
-      level: 1,
-      reqxp: 100
+    if (!xpfile[message.author.id]) {
+      xpfile[message.author.id] = {
+        xp: 0,
+        level: 1,
+        reqxp: 100
+      }
+
+      fs.writeFile("./database/xp.json", JSON.stringify(xpfile), function (err) {
+        if (err) console.log(err)
+      })
     }
 
+    xpfile[message.author.id].xp += addXP
+
+    if (xpfile[message.author.id].xp > xpfile[message.author.id].reqxp) {
+      xpfile[message.author.id].xp -= xpfile[message.author.id].reqxp
+      xpfile[message.author.id].reqxp *= 1.25
+      xpfile[message.author.id].reqxp = Math.floor(xpfile[message.author.id].reqxp)
+      xpfile[message.author.id].level += 1
+
+      let member = message.mentions.users.first() || message.author
+
+      const embed = new MessageEmbed()
+        .setColor('RANDOM')
+        .setTitle(`${member.tag}`)
+        .setDescription("You Are Now Level **" + xpfile[message.author.id].level + "**!")
+        .setImage('https://emoji.gg/assets/emoji/9104-nekodance.gif')
+      message.channel.send(embed).then(embed => { embed.delete({ timeout: 10000 }) })
+
+    }
     fs.writeFile("./database/xp.json", JSON.stringify(xpfile), function (err) {
       if (err) console.log(err)
     })
-  }
-
-  xpfile[message.author.id].xp += addXP
-
-  if (xpfile[message.author.id].xp > xpfile[message.author.id].reqxp) {
-    xpfile[message.author.id].xp -= xpfile[message.author.id].reqxp
-    xpfile[message.author.id].reqxp *= 1.25
-    xpfile[message.author.id].reqxp = Math.floor(xpfile[message.author.id].reqxp)
-    xpfile[message.author.id].level += 1
-
-    let member = message.mentions.users.first() || message.author
-
-    const embed = new MessageEmbed()
-      .setColor('RANDOM')
-      .setTitle(`${member.tag}`)
-      .setDescription("You Are Now Level **" + xpfile[message.author.id].level + "**!")
-      .setImage('https://emoji.gg/assets/emoji/9104-nekodance.gif')
-    message.channel.send(embed).then(embed => { embed.delete({ timeout: 10000 }) })
-
-  }
-  fs.writeFile("./database/xp.json", JSON.stringify(xpfile), function (err) {
-    if (err) console.log(err)
-  })
-});
-const distube = require('distube');
-client.distube = new distube(client, { searchSongs: false, emitNewSongOnly: true , leaveOnEmpty: true , leaveOnFinish: true })
-client.distube
+  });
+  const distube = require('distube');
+  client.distube = new distube(client, { searchSongs: false, emitNewSongOnly: true, leaveOnEmpty: true, leaveOnFinish: true })
+  client.distube
     .on('playSong', (message, queue, song) => message.channel.send(
-        `Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user}`,
+      `Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user}`,
     ))
     .on('addSong', (message, queue, song) => message.channel.send(
-        `Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`,
+      `Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`,
     ))
     .on('error', (message, e) => {
-		message.channel.send(`An error encountered: ${e}`)
-	})
-  .on('empty', (message, queue) => {
-		message.channel.send(`***Channel is empty. Leaving the channel***`)
-  })
-  .on('finish', (message, queue) => {
-		message.channel.send(`***Finish queue. Leaving the channel***`)
-  });
-if (!skip) {
-  client.login(config.token);
+      message.channel.send(`An error encountered: ${e}`)
+    })
+    .on('empty', (message, queue) => {
+      message.channel.send(`***Channel is empty. Leaving the channel***`)
+    })
+    .on('finish', (message, queue) => {
+      message.channel.send(`***Finish queue. Leaving the channel***`)
+    });
+
+    client.login(config.token);
+  } else {
+    console.error('[ERROR]','Please spectify a Discord bot token in config.json.')
+    //eslint-disable-next-line no-unreachable
+    process.exit(1)
+  }
 }
+async function update() {
+  if (config.autoUpdate == true) {
+    console.log('[Git]', 'Updating...')
+    const child = childProcess.spawn('git', ['pull'], {
+      stdio: 'inherit'
+    })
+    //ensure thats the bot run after update.
+    child.on('close', async () => {
+      await main()
+    })
+  } else {
+    await main()
+  }
+}
+update()
