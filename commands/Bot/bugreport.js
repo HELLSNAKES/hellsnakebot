ca47c65b648f7302e1936dd17a603c2802a0f439
HELLSNAKE <63496374+HELLSNAKES@users.noreply.github.com>
2021-08-03 17:49:23 +0700

    bugreport command

---
+++ commands/Bot/bugreport.js
@@ -0,0 +1,27 @@
+const { Client, Message, MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: "bugreport",
+    aliases : ["bug"],
+    category : "Bot",
+    description : "Send a message to Admin",
+    timeout: 300000,
+    usage: "[command]+[text]",
+    run: async(client, message, args) => {
+       const admin = client.users.cache.get(require('../../config.json').Admin);
+       const bug = args.join(" ");
+       if(!bug) return message.channel.send('Please specify a bug!');
+
+       const embed = new MessageEmbed()
+       .setTitle('Bug report')
+       .setColor('RANDOM')
+       .addField('Username', message.author.toString(), true)
+       .addField('ServerName', message.guild.name ,true)
+       .addField('ServerID', message.guild.id , true)
+       .addField('Bug', bug)
+       .setThumbnail(message.author.displayAvatarURL())
+       .setTimestamp()
+       message.reply(`***Thanks for finding the bug! :)***`);
+       admin.send(embed) 
+    }
+}
