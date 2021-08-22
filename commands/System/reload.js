f1c6c3f6844a204536009d456399450c3cceeb9d
allehS <69383963+Hiyoriii@users.noreply.github.com>
2021-08-22 20:53:37 +0700

    reload khong can category

--- commands/System/reload.js
+++ commands/System/reload.js
@@ -9,9 +9,9 @@ module.exports = {
     if(message.author.id !== Admin) return message.channel.send('Insufficient permission!!')
     if (!args[0]) return message.channel.send('Give a category');
     if (!args[1]) return message.channel.send('Give a command');
-   
-    let category = args[0].toLowerCase();
-    let command = args[1].toLowerCase();
+    var commandinfo = client.commands.get(command);
+    var category = commandinfo.category
+    let command = args[0].toLowerCase();
     
     try {
         delete require.cache[require.resolve(`../../commands/${category}/${command}.js`)];
