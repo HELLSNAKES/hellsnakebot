46fd775ed8325e44e887c3ea434090f20f428f11
HELLSNAKE <63496374+HELLSNAKES@users.noreply.github.com>
2021-08-22 21:17:21 +0700

    volume

---
+++ commands/Music/volume.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const progressbar = require('string-progressbar');
+
+module.exports = {
+    name: "volume",
+    category : "music",
+    aliases : ['v'],
+    description : "Change the music player's volume.",
+    timeout: 3000,
+    usage: '[command]+[amount]',
+    run: async(client, message, args) => {
+        if(!message.member.voice.channel)
+         return message.reply('Please join a voice channel!')
+        let amount = parseInt(args[0]);
+        if(!amount) return message.channel.send("Please specify a volume.")
+        if(amount > 100) {
+            message.channel.send("Please enter a valid number (between 1 and 100)")
+        } else {
+            client.distube.setVolume(message, amount);
+            var total = 100;
+            var current = amount;
+            let bar = progressbar.splitBar(total, current, 30, "▬", "🔘")[0];
+            message.channel.send(`Set the new volume to ${amount}%.`);
+            message.channel.send(bar);
+        }
+    }
+}
