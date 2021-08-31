4226fa59a99c81546773bf803c54a59e03fa7510
HELLSNAKE <63496374+HELLSNAKES@users.noreply.github.com>
2021-08-31 11:27:23 +0700

    exec command

---
+++ commands/System/exec.js
@@ -0,0 +1,20 @@
+const childProcess = require('child_process');
+const { Admin } = require('../../config.json');
+
+module.exports = {
+  name: "exec",
+  category: "System",
+  description: "Executes a process command.",
+  usage: "[command] + [process command]",
+  run: async (client, message, args) => {
+    if(message.author.id !== Admin) 
+    return message.channel.send('Insufficient permission!!')
+    if (!args[0])
+    return message.channel.send('Usage [command] + [process command]');
+     childProcess.exec(args.join(' '), {},
+        (err, stdout, stderr) => {
+            if (err) return message.channel.send('```' + err.message + '```');
+            message.channel.send('```' + stdout + '```');
+      })
+   }    
+}
