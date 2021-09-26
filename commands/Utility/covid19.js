af4f717d654383e57219655825af6c6266f81bb3
meimei <91164564+meimeiowo@users.noreply.github.com>
2021-09-26 09:26:26 +0700

    Update covid19.js

--- commands/Utility/covid19.js
+++ commands/Utility/covid19.js
@@ -2,33 +2,49 @@ const fetch = require('node-fetch');
 const { MessageEmbed } = require('discord.js')
 
 module.exports = {
-    name : 'covid',
+    name: 'covid19',
     category: "Utility",
     description: "Covid19 search",
-    usage: "[command] + [countries]",
-    author: "[CuSO4-c3c,Hiyoriii,Hellsnakes]",
-    run: async(client, message, args) => {
-		 const countries = args.join(" ");
-         if(!countries) return message.channel.send('Usage [command]+[countries]')
-         const url = `https://corona.lmao.ninja/v2/countries/${countries}`
-         let response
-           try{
-               response = await fetch(url).then(res => res.json())
-           }
-           catch(e) {
-               return message.reply('An Error Occured, Try Again Later.')
-           }
-           const embed = new MessageEmbed()
-           .setTitle(`COVID-19 Stats for **${response.country}**`)
-           .setColor("RANDOM")
-           .setThumbnail(response.countryInfo.flag)
-           .addField('Total Cases', response.cases.toLocaleString())
-           .addField('Total Deaths', response.deaths.toLocaleString())
-           .addField('Total Recovered', response.recovered.toLocaleString())
-           .addField('Active Cases', response.active.toLocaleString())
-           .addField('Critical Cases', response.critical.toLocaleString())
-           .addField('Todays Deaths', response.todayDeaths.toLocaleString())
-           .addField('Today Recoveries', response.todayRecovered.toLocaleString())
-           message.channel.send(embed)
-       }
+    usage: "[command] + [countries] | all",
+    author: "[Harmonynos Team]",
+    run: async (client, message, args) => {
+        const countries = args.join(" ");
+        if (!args[0])
+            return message.channel.send('Usage [command] + all | countries');
+        if (args[0] === "all") {
+            const url = `https://disease.sh/v3/covid-19/all`
+            let response
+            response = await fetch(url).then(res => res.json())
+            const embed = new MessageEmbed()
+                .setTitle(`Worldwide COVID-19 Stats 🌎`)
+                .setThumbnail(`https://i.imgur.com/dJ52ckG.jpg`)
+                .addField('Total Cases', response.cases.toLocaleString())
+                .addField('Total Deaths', response.deaths.toLocaleString())
+                .addField('Total Recovered', response.recovered.toLocaleString())
+                .addField('Active Cases', response.active.toLocaleString())
+                .addField('Critical Cases', response.critical.toLocaleString())
+                .addField('Todays Deaths', response.todayDeaths.toLocaleString())
+                .addField('Today Recoveries', response.todayRecovered.toLocaleString())
+            message.channel.send(embed)
+        } else {
+            const url = `https://disease.sh/v3/covid-19/countries/${encodeURIComponent(countries)}`
+            let response
+            response = await fetch(url).then(res => res.json())
+            try {
+                const embed = new MessageEmbed()
+                    .setTitle(`COVID-19 Stats for **${countries}**`)
+                    .setThumbnail(response.countryInfo.flag)
+                    .addField('Total Cases', response.cases.toLocaleString())
+                    .addField('Total Deaths', response.deaths.toLocaleString())
+                    .addField('Total Recovered', response.recovered.toLocaleString())
+                    .addField('Active Cases', response.active.toLocaleString())
+                    .addField('Critical Cases', response.critical.toLocaleString())
+                    .addField('Todays Deaths', response.todayDeaths.toLocaleString())
+                    .addField('Today Recoveries', response.todayRecovered.toLocaleString())
+                message.channel.send(embed)
+            } catch {
+                message.channel.send('Country not found')
+            }
+        }
+    }
 }
